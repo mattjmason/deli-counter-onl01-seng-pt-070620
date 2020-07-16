d968eeafa3fa2864ad569ebaacca98bb3b406d8f
Matt Mason <masonmj10@gmail.com>
2020-07-16 03:28:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,11 +6,11 @@ def line(array)
   if array.count == 0 
     puts "The line is currently empty."
   else 
-    response = "The line is currently:"
+    place = "The line is currently:"
     array.each_with_index do |name,index| 
-    response += " #{index+1}. #{name}"
+    place += " #{index+1}. #{name}"
   end
-  puts response
+  puts place
 end  
 end
     
